f97cf68f04fa21db7a62cf3744a3e0cc849aff21
viniciussluiz <103579574+viniciussluiz@users.noreply.github.com>
2023-03-23 22:30:13 -0300

    Mudança pagina principal

--- app/src/Principal.js
+++ app/src/Principal.js
@@ -22,7 +22,7 @@ export default function Principal({ navigation }) {
             color="white"
           />
         }
-        title=" Log in"
+        title=" Próxima página"
         onPress={() => entrar()}
       />
     </View>
