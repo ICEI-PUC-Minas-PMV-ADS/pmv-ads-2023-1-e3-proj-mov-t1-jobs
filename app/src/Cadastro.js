9fa358a33d8ad47d7283aea31956b2d8c2146c25
viniciussluiz <103579574+viniciussluiz@users.noreply.github.com>
2023-04-08 01:24:35 -0300

    checkbox na tela de cadastro

--- app/src/Cadastro.js
+++ app/src/Cadastro.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Image } from 'react-native';
-import { Button, Input, Text } from 'react-native-elements';
+import { Button, CheckBox, Input, Text } from 'react-native-elements';
 import Icon  from 'react-native-vector-icons/FontAwesome';
 import styles from '../style/MainStyle';
 
@@ -11,6 +11,7 @@ export default function Cadastro({navigation}) {
   const [nome, setNome] = useState(null)
   const [cpf, setCpf] = useState(null)
   const [telefone, setTelefone] = useState(null)
+  const [isSelected, setSelected] = useState(false)
 
   const salvar = () => {
     console.log("Salvou")
@@ -43,6 +44,16 @@ export default function Cadastro({navigation}) {
         onChangeText={value => setEmail(value)}
         keyboardType="phone-pad"
         returnKeyType="done"
+      />
+
+      <CheckBox
+          title="Eu aceito os termos de uso"
+          checkedIcon="check"
+          uncheckedIcon="square-o"
+          checkedColor="green"
+          uncheckedColor="red"
+          checked={isSelected}
+          onPress={() => setSelected(!isSelected)}
       />
 
       <Button 
